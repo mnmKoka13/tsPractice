f19091c1e8821939af6668144ebdb7d187abc43e
Manami Kokado <kokado313.mnm@gmail.com>
2025-04-05 14:27:53 +0900

    真偽値リテラル、null、undefined

--- src/index.ts
+++ src/index.ts
@@ -56,3 +56,17 @@ console.log(result);
 
 // const wrong = 100n + 50;  // bigInt と number の混在はエラーになる
 
+// ==================
+
+const no: boolean = false;
+const yes: boolean = true;
+console.log(yes, no);
+
+// ==================
+
+const val1: null = null;
+const val2: undefined = undefined;
+
+console.log(val1, val2);
+
+// ==================
